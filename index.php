bc01460616b9604416fb8eebc449d31831fe597d
Vahid Mohammadi <email@vmoh.ir>
2018-03-22 17:27:03 +0430

    send message to tbot

--- index.php
+++ index.php
@@ -4,11 +4,15 @@ use PHPHtmlParser\Dom;
 
 $dom = new Dom;
 $dom->loadFromUrl('https://dribbble.com/shots/4374190-Hello-Dribbble');
+$boturl = 'https://api.telegram.org/bot571854722:AAGG7U3jYeW0B6xM5aH9UIw5Xbt0L9EgZ3s/';
+$sendmessage = $boturl . 'sendMessage?chat_id=92454&text=';
 // $html = $dom->outerHtml;
 
 $res = $dom->find('span.views-count');
-echo intval(str_replace(' views', '', str_replace(',', '', $res[0]->innerHtml()))) . PHP_EOL;
+$views = intval(str_replace(' views', '', str_replace(',', '', $res[0]->innerHtml()))) . PHP_EOL;
 $res = $dom->find('a.likes-count');
-echo intval(str_replace(' likes', '', str_replace(',', '', $res[0]->innerHtml())));
+$likes = intval(str_replace(' likes', '', str_replace(',', '', $res[0]->innerHtml())));
+
+file_get_contents($sendmessage . $views . '-' . $likes)
 
 ?>
