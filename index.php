3cdf70f56ebe925cd58849c98c165552612bb150
Vahid Mohammadi <email@vmoh.ir>
2018-03-24 02:14:25 +0430

    150 1000

--- index.php
+++ index.php
@@ -1,20 +1,15 @@
 <?php
 error_reporting(-1);
 ini_set('display_errors', 'On');
+
 // requirements
 require 'vendor/autoload.php';
 require_once 'config.php';
 // file_get_contents('https://api.telegram.org/bot'.$token.'/sendMessage?chat_id=92454&text=debug');
-// require 'main-controller.php';
 use PHPHtmlParser\Dom;
 use Telegram\Bot\Api;
 use Telegram\Bot\FileUpload\InputFile;
-// connecting
 $telegram = new Api($token);
-// get user message
-// $updates = $telegram->getWebhookUpdates();
-// $message = $updates->getMessage();
-// $callback_query = $updates->getCallbackQuery();
 
 if (isset($_GET['debug'])) {
     log_debug($_GET['debug']);
@@ -54,35 +49,26 @@ function log_debug($data, $chat_id = 92454)
     ]);
 }
 
-// if ($message != null) {
-// $chat = $message->getChat();
-// $chat_id = (int) $chat->getId();
-// $text       = $message->getText();
-// $message_id = $message->getMessageId();
-// $username   = $message->getFrom()->getUsername();
-// $user       = $message->getFrom();
-// $username   = $user->getUsername();
-// $fullname   = $user->getFirstName() . ' ' . $user->getLastName();
 try {
     $dom = new Dom;
     $boturl = 'https://api.telegram.org/bot' . $token . '/';
     $sendmessage = $boturl . 'sendMessage?chat_id=' . $admin_id . '&text=';
 
     $json = json_decode(file_get_contents('php://input'), true);
-    // var_export($json);
+    log_debug($json);
 
     $dom->loadFromUrl($json['url']);
     $likes = getLikes();
     $views = getViews();
 
     $chatID = $admin_id;
-    if ($likes > 200 || $views > 2000) {
+    if ($likes > 150 || $views > 1000) {
         $chatID = $channel_id;
     }
-    $titleStr = '🔅 <a href="' . $json['url'] . '">' . $json['title'] . '</a>';
+    $titleStr = '🔅 ' . $json['title'];
     $likeStr = trim('👍 Likes: ' . $likes);
     $viewStr = trim('👀 Views: ' . $views);
-    $idStr = '💠 @inspired_design';
+    $idStr = '💠 ' . $channel_id;
     $caption = $titleStr . PHP_EOL . $likeStr . PHP_EOL . $viewStr . PHP_EOL . $idStr;
 
     if (strlen($caption) > 200) {
@@ -99,7 +85,7 @@ try {
         'chat_id' => $chatID,
         'photo' => InputFile::create($json['image'], 't.me/inspired_design.jpg'),
         'caption' => $caption,
-        'parse_mode' => 'html',
+        // 'parse_mode' => 'html',
     ];
     var_export($sMessage);
     $telegram->sendPhoto($sMessage);
