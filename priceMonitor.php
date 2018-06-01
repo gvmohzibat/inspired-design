872725f842476ecf766331373937082da1491942
Vahid Mohammadi <email@vmoh.ir>
2018-06-01 23:25:21 +0430

    priceMonitor

---
+++ priceMonitor.php
@@ -0,0 +1,66 @@
+<?php
+error_reporting(-1);
+ini_set('display_errors', 'On');
+
+require 'vendor/autoload.php';
+require_once 'config.php';
+
+use Telegram\Bot\Api;
+$telegram = new Api($token);
+
+function make_exception_array($e)
+{
+    return [
+        'exception' => 'exception',
+        'file' => $e->getFile(),
+        'line' => $e->getLine(),
+        'message' => $e->getMessage(),
+        'code' => $e->getCode(),
+        'trace' => $e->getTraceAsString(),
+    ];
+}
+
+function dbg($data, $chat_id = 92454)
+{
+    $text = var_export($data, true);
+    global $telegram;
+    $telegram->sendMessage([
+        'chat_id' => $chat_id,
+        'text' => $text,
+    ]);
+}
+
+$telegram->sendMessage([
+    'chat_id' => 92454,
+    'text' => 'vahid',
+]);
+
+try {
+    $urls = [
+        'http://torob.com/p/254b8b34-1e0c-40b2-830e-ad49705ee9fb/%DA%AF%D9%88%D8%B4%DB%8C-%D9%85%D9%88%D8%A8%D8%A7%DB%8C%D9%84-%D9%87%D9%88%D8%A7%D9%88%DB%8C-%D9%85%D8%AF%D9%84-mate-10-alp-l29-%D8%AF%D9%88-%D8%B3%DB%8C%D9%85-%DA%A9%D8%A7%D8%B1%D8%AA/',
+        'https://www.digikala.com/Product/DKP-32334/',
+    ];
+
+    foreach ($urls as $url) {
+        // echo '<br>';
+        // var_export($url);
+        // echo '<br>';
+
+        $dom = new DOMDocument();
+        libxml_use_internal_errors(1);
+        @$dom->loadHTML(file_get_contents($url));
+
+        $div = $dom->getElementsByTagName('script');
+
+        foreach ($div as $d) {
+            if ($d->getAttribute('type') == 'application/ld+json') {
+                dbg(json_decode($d->textContent, true)['offers']['lowPrice']);
+                dbg(json_decode($d->textContent, true)['offers']['price']);
+            }
+        }
+    }
+} catch (Exception $e) {
+    dbg(make_exception_array($e));
+}
+?>
+</pre>
